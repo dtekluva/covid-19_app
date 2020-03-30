486ba3b125c30e7f0714dfd233f2b33391666b73
Nextafari <chinedue16@gmail.com>
2020-03-30 21:59:44 +0100

    modified version

--- sql_with_python.py
+++ sql_with_python.py
@@ -12,14 +12,14 @@ connection = pymysql.connect (host = "localhost",
 def create_tables():
 
     with connection.cursor() as cursor:
-        table_for_countries = "create table countries (id int primary key not null auto_increment, name varchar(100), longitude float, latitude float)"
+        table_for_countries = "create table if not exists countries (id int primary key not null auto_increment, name varchar(100), longitude float, latitude float)"
         cursor.execute(table_for_countries)
 
-        table_for_deaths = "create table deaths (id int primary key not null auto_increment, country_id int(100), foreign key(country_id) references countries(id), deaths int, date_of_death date)"
+        table_for_deaths = "create table if not exists deaths (id int primary key not null auto_increment, country_id int(100), foreign key(country_id) references countries(id), deaths int, date_of_death date)"
         cursor.execute(table_for_deaths)
 
         connection.commit()
-#create_tables()
+
 
 
 
@@ -60,11 +60,13 @@ def check_country(name):
 #This function formats the date in the csv file into sql format of year, month and day
 def date_format(date):
     month, day, year = date.split("/")      #spliting the dates in the csv file into a list eg 1/20/20 now becomes [1, 20, 20]
+    
     #using a - to separate the list([1, 20, 20]), flipping the list while concatenating +20 to the year(20) to have ([2020-1-20])
     changed_date = "-".join([year.replace('\n', '')+'20', month, day])  
+    
     return changed_date
 
 
-#write_countries()
+create_tables()
 
 
